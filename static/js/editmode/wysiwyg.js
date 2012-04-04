0537fd1d996f7f9ff792834a6b2657549980c030
Nikolaus Flamann <nikolaus@flamann.at>
2012-04-04 21:32:02 +0200

    General Logic, Simple Dokument Elements

---
+++ static/js/editmode/wysiwyg.js
@@ -0,0 +1,14 @@
+var MyCustomToolbar = [
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        '/',
+        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['Link','Unlink','Anchor'],
+        ['Image','Flash','Table','HorizontalRule','SpecialChar','PageBreak'],
+        '/',
+        ['Styles','Format','Font','FontSize'],
+        ['TextColor','BGColor'],
+        ['Maximize', 'ShowBlocks','Source']
+    ];
--- website/views/scripts/content/starter/wysiwyg.php
+++ website/views/scripts/content/starter/wysiwyg.php
@@ -11,7 +11,38 @@ $this->layout()->setLayout("default");
 
 <div class="contentblock">
     <?php
+    /**
+     * Simple Text
+     */
     echo $this->wysiwyg("simplecontent");
+
+
+    /**
+     * Advanced Use
+     */
+
+    $demotoolbar = <<<EOD
+var DemoToolbar = [
+    ['Cut', 'Copy', 'Paste', 'PasteText',  '-',],
+    ['Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat'],
+    '/',
+    ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
+    ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
+    ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+    ['Link', 'Unlink', 'Anchor'],
+    ['Image', 'Table', 'HorizontalRule', 'SpecialChar', 'PageBreak'],
+];
+EOD;
+
+    $this->headScript()->appendScript($demotoolbar);
+
+
+    echo $this->wysiwyg("content", array(
+        "width" => 640,
+        "height" => 500,
+        "toolbar" => "DemoToolbar",
+    ));
+
     ?>
 </div>
 
